7b2749a1f2c2121c2fefee987d027969359481d0
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-06-20 08:17:19 +0300

    Add solution for 01

---
+++ django_models_basics_lab/01_employee_model.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=30)
+    email_address = models.EmailField()
+    photo = models.URLField()
+    birth_date = models.DateField()
+    works_full_time = models.BooleanField()
+    created_on = models.DateTimeField(auto_now_add=True)
