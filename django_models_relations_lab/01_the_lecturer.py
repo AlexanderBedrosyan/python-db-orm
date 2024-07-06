4dff6264704472295976f69a5b6c1c881b57ea0b
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-07-07 01:47:52 +0300

    Add solution for 01

---
+++ django_models_relations_lab/01_the_lecturer.py
@@ -0,0 +1,25 @@
+# In models.py
+
+from django.db import models
+from datetime import date
+
+# Create your models here.
+
+
+class Lecturer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+    lecturer = models.ForeignKey(
+        to=Lecturer,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
