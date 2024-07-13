79ee344613a59745392f32aabb6fd5fa7479726f
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-07-14 00:31:53 +0300

    Add solution for 05

---
+++ models_inheritance_and_customization_lab/05_animal_display_system_logic.py
@@ -0,0 +1,27 @@
+# In models.py
+from django.core.exceptions import ValidationError
+from django.db import models
+from datetime import date
+
+# Create your models here.
+
+
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    sound = models.CharField(max_length=100)
+
+
+class ZooDisplayAnimal(Animal):
+
+    def display_info(self):
+        return f"Meet {self.name}! Species: {self.species}, born {self.birth_date}. It makes a noise like '{self.sound}'."
+
+    def is_endangered(self):
+        if self.species in ["Cross River Gorilla", "Orangutan", "Green Turtle"]:
+            return f"{self.species} is at risk!"
+        return f"{self.species} is not at risk."
+
+    class Meta:
+        proxy = True
