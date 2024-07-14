36575b770154fbc1b6423a5c93c88844826319ab
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-07-15 00:05:27 +0300

    Add solution for 01

---
+++ models_inheritance_and_customization_exercise/01_character_classes.py
@@ -0,0 +1,66 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+from datetime import date
+
+# Create your models here.
+
+from django.db import models
+
+
+class BaseCharacter(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta():
+        abstract = True
+
+
+class Mage(BaseCharacter):
+
+    elemental_power = models.CharField(max_length=100)
+    spellbook_type = models.CharField(max_length=100)
+
+
+class Assassin(BaseCharacter):
+
+    weapon_type = models.CharField(max_length=100)
+    assassination_technique = models.CharField(max_length=100)
+
+
+class DemonHunter(BaseCharacter):
+
+    weapon_type = models.CharField(max_length=100)
+    demon_slaying_ability = models.CharField(max_length=100)
+
+
+class TimeMage(Mage):
+
+    time_magic_mastery = models.CharField(max_length=100)
+    temporal_shift_ability = models.CharField(max_length=100)
+
+
+class Necromancer(Mage):
+
+    raise_dead_ability = models.CharField(max_length=100)
+
+
+class ViperAssassin(Assassin):
+
+    venomous_strikes_mastery = models.CharField(max_length=100)
+    venomous_bite_ability = models.CharField(max_length=100)
+
+
+class ShadowbladeAssassin(Assassin):
+
+    shadowstep_ability = models.CharField(max_length=100)
+
+
+class VengeanceDemonHunter(DemonHunter):
+
+    vengeance_mastery = models.CharField(max_length=100)
+    retribution_ability = models.CharField(max_length=100)
+
+
+class FelbladeDemonHunter(DemonHunter):
+
+    felblade_ability = models.CharField(max_length=100)
