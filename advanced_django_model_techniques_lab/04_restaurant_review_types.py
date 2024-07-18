985cce0416f84f6921b1a6d6136b6e367219adf2
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-07-18 17:07:25 +0300

    Add solution for 04

---
+++ advanced_django_model_techniques_lab/04_restaurant_review_types.py
@@ -0,0 +1,72 @@
+from django.core import validators
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator, MaxValueValidator
+from django.db import models
+from datetime import date, datetime, timedelta
+
+# Create your models here.
+
+from django.db import models
+
+
+class Restaurant(models.Model):
+    name = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(2, message="Name must be at least 2 characters long."),
+            MaxLengthValidator(100, message="Name cannot exceed 100 characters.")
+        ]
+    )
+    location = models.CharField(
+        max_length=200,
+        validators=[
+            MinLengthValidator(2, message="Location must be at least 2 characters long."),
+            MaxLengthValidator(200, message="Location cannot exceed 200 characters.")
+        ]
+    )
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+    rating = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0.00, message="Rating must be at least 0.00."),
+            MaxValueValidator(5.00, message="Rating cannot exceed 5.00.")
+        ]
+    )
+
+
+class RestaurantReview(models.Model):
+    reviewer_name = models.CharField(max_length=100)
+    restaurant = models.ForeignKey(
+        to=Restaurant,
+        on_delete=models.CASCADE
+    )
+    review_content = models.TextField()
+    rating = models.PositiveIntegerField(
+        validators=[validators.MaxValueValidator(limit_value=5)]
+    )
+
+    class Meta:
+        ordering = ['-rating']
+        verbose_name = 'Restaurant Review'
+        verbose_name_plural = 'Restaurant Reviews'
+        unique_together = ['reviewer_name', 'restaurant']
+        abstract = True
+
+
+class RegularRestaurantReview(RestaurantReview):
+    pass
+
+
+class FoodCriticRestaurantReview(RestaurantReview):
+    food_critic_cuisine_area = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['-rating']
+        verbose_name = 'Food Critic Review'
+        verbose_name_plural = 'Food Critic Reviews'
+        unique_together = ['reviewer_name', 'restaurant']
+
