5aad63871650af1570e714221d8c349604b71740
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-07-23 00:03:04 +0300

    Add solution for 01

---
+++ advanced_queries_in_django_exercise/01_real_estate_listing.py
@@ -0,0 +1,45 @@
+from decimal import Decimal
+from django.core import validators
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator, MaxValueValidator, \
+    RegexValidator
+from django.db import models
+from datetime import date, datetime, timedelta
+import re
+
+# Create your models here.
+
+from django.db import models
+from django.db.models import QuerySet, Q, Count
+
+
+class RealEstateListingManager(models.Manager):
+    def by_property_type(self, property_type: str) -> QuerySet:
+        return self.filter(property_type=property_type)
+
+    def in_price_range(self, min_price: Decimal, max_price: Decimal) -> QuerySet:
+        query = Q(price__gte=min_price) & Q(price__lte=max_price)
+        return self.filter(query)
+
+    def with_bedrooms(self, bedrooms_count: int) -> QuerySet:
+        return self.filter(bedrooms=bedrooms_count)
+
+    def popular_locations(self):
+        return self.values('location').annotate(location_count=Count('location')).order_by('-location_count', 'location')[:2]
+
+
+class RealEstateListing(models.Model):
+    PROPERTY_TYPE_CHOICES = [
+        ('House', 'House'),
+        ('Flat', 'Flat'),
+        ('Villa', 'Villa'),
+        ('Cottage', 'Cottage'),
+        ('Studio', 'Studio'),
+    ]
+
+    property_type = models.CharField(max_length=100, choices=PROPERTY_TYPE_CHOICES)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    bedrooms = models.PositiveIntegerField()
+    location = models.CharField(max_length=100)
+
+    objects = RealEstateListingManager()
